47770f4d029f2d9670a96503f475106e8ec310f3
vitoorcruz2004 <vitoorcruz2004@gmail.com>
2026-06-10 11:04:39 -0300

    deploy: lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+export const supabase = createClient(url, anon)
+
+export function supabaseServer() {
+  return createClient(url, process.env.SUPABASE_SERVICE_KEY!)
+}
