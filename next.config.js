c51cecd0f6812921173b48053517db5fd82f9b40
vitoorcruz2004 <vitoorcruz2004@gmail.com>
2026-06-11 10:52:37 -0300

    feat: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'enem.dev' },
+      { protocol: 'https', hostname: '*.enem.dev' },
+      { protocol: 'https', hostname: 'enem-api-assets.s3.amazonaws.com' },
+    ],
+  },
+}
 module.exports = nextConfig
